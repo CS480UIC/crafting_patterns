02ffa24b6b6213d1bf4aa7912c3e5c5b379e2cb8
Adam Ciuris <adamciuris@gmail.com>
2022-03-29 21:18:41 -0500

    deletedb and fix csv

---
+++ sql/deleteDB.sql
@@ -0,0 +1,8 @@
+DROP TABLE user;
+DROP TABLE entity1;
+DROP TABLE knitting_pattern_yarn;
+DROP TABLE crochet_pattern_yarn;
+DROP TABLE crochet_pattern;
+DROP TABLE knitting_pattern;
+DROP TABLE pattern_author;
+DROP TABLE yarn;
