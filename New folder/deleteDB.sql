558fcab6cb1e5f20565fc1434006062ebbfb55f2
Adam Ciuris <adamciuris@gmail.com>
2022-04-01 20:44:42 -0500

    renamed bookstore to patterns

--- New folder/deleteDB.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE user;
-DROP TABLE entity1;
-DROP TABLE knitting_pattern_yarn;
-DROP TABLE crochet_pattern_yarn;
-DROP TABLE crochet_pattern;
-DROP TABLE knitting_pattern;
-DROP TABLE pattern_author;
-DROP TABLE yarn;
